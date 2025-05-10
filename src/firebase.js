359b59117f552d665cd1f9ae52466e37226c10b3
Ron2Avraham <roninthespot@gmail.com>
2025-05-10 21:22:35 +0300

    Change to env

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,31 @@ import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAEdXXMRTR6gcbVRpYdTSnXtagAf7O2N1g",
-  authDomain: "giftme-94370.firebaseapp.com",
-  projectId: "giftme-94370",
-  storageBucket: "giftme-94370.firebasestorage.app",
-  messagingSenderId: "257741744238",
-  appId: "1:257741744238:web:f50ee2f16e11bb20a19e7f",
-  measurementId: "G-RTBZCY17WJ"
+apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+appId: process.env.REACT_APP_FIREBASE_APP_ID,
+measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
+// Validate required environment variables
+const requiredEnvVars = [
+'REACT_APP_FIREBASE_API_KEY',
+'REACT_APP_FIREBASE_AUTH_DOMAIN',
+'REACT_APP_FIREBASE_PROJECT_ID'
+];
+
+const missingEnvVars = requiredEnvVars.filter(
+envVar => !process.env[envVar]
+);
+
+if (missingEnvVars.length > 0) {
+throw new Error(
+  `Missing required environment variables: ${missingEnvVars.join(', ')}`
+);
+}
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
